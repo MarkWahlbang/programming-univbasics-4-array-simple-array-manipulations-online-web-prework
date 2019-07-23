d8a012b36f8b3598d765e1dfe7650e393f105040
brady sisk <bradysiskpro@yahoo.com>
2019-07-23 20:02:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,5 +23,6 @@ houston = ["watt","hopkins"]
 rams = ["gurley", "goff", "donald", "suh"]
 rams.insert("kupp")
 
-rams = ["gurley", "goff"] ["donald", "suh"]
+rams =
+["gurley", "goff"] ["donald", "suh"]
 rams.flatten
