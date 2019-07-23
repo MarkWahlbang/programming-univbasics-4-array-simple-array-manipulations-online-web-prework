686a025f51fc2e5652c7cb6c25ae9a7b6e850d2a
brady sisk <bradysiskpro@yahoo.com>
2019-07-23 19:43:12 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,21 @@
+rams = ["gurley", "goff", "donald", "suh"]
+rams.push("kupp")
+
+rams = ["gurley", "goff", "donald", "suh"]
+rams.unshift("kupp")
+
+rams = ["gurley", "goff", "donald", "suh"]
+rams.pop
+
+rams = ["gurley", "goff", "donald", "suh"]
+rams.pop(2)
+
+rams = ["gurley", "goff", "donald", "suh"]
+rams.shift
+
+rams = ["gurley", "goff", "donald", "suh"]
+rams.shift(2)
+
+rams = ["gurley", "goff", "donald", "suh"]
+houston.concat["watt","hopkins"]
+
